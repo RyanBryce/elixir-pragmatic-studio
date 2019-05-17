b3800a413a11f20e005231c7a24d3ddec455316c
Ryan Bryce <ryan.bryce1992@gmail.com>
2019-05-16 21:29:32 -0600

    function clause lesson

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -2,9 +2,11 @@ defmodule Servy.Handler do
   def handle(request) do
       request
         |> parse
+        |> log
         |> route
         |> format_response
   end
+  def log(conv),  do: IO.inspect conv
 
   def parse(request) do
     # TODO: Parse the request string into a map:
@@ -19,9 +21,16 @@ defmodule Servy.Handler do
 
   def route(conv) do
     # TODO: Create a new map that also has the response body:
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    route(conv, conv.method, conv.path)
   end
 
+  def route(conv, "GET", "/wildthings") do
+    %{conv | resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{conv | resp_body: "Teddy, Smokey, Paddington"}
+  end
   def format_response(conv) do
     # TODO: Use values in the map to create an HTTP response string:
     """
@@ -36,6 +45,30 @@ end
 
 request = """
   GET /wildthings HTTP/1.1
+  Host: example.com
+  User-Agent: ExampleBrowser/1.0
+  Accept: */*
+
+  """
+
+  response = Servy.Handler.handle(request)
+
+  IO.puts response
+
+  request = """
+  GET /bears HTTP/1.1
+  Host: example.com
+  User-Agent: ExampleBrowser/1.0
+  Accept: */*
+
+  """
+
+  response = Servy.Handler.handle(request)
+
+  IO.puts response
+
+  request = """
+  GET /bigfoot HTTP/1.1
   Host: example.com
   User-Agent: ExampleBrowser/1.0
   Accept: */*
