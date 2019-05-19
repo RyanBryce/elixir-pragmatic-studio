531c4ccd8747314032bd0855878b6ea54221b5f4
Ryan Bryce <ryan.bryce1992@gmail.com>
2019-05-18 23:33:59 -0600

    added structs and learned to read and write to them and how to specify a struct over a map on pattern matching

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -1,12 +1,14 @@
 defmodule Servy.Handler do
   @moduledoc "Handles 'HTTP' requests"
 
-  @pages_path Path.expand("../../pages", __DIR__)
+  @pages_path Path.expand("pages", File.cwd!)
 
   # import funcitons, only specifies only the functions we want and the arrity of that func
 
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
+  import Servy.FileHandler, only: [handle_file: 2]
+  alias Servy.Conv
 
   @doc "transforms a request into a resp"
   def handle(request) do
@@ -19,50 +21,39 @@ defmodule Servy.Handler do
       |> format_response
   end
 
-  def handle_file({:ok, content}, conv ) do
-    %{conv | status: 200, resp_body: content}
-  end
-
-  def handle_file({:error, :enoent}, conv ) do
-    %{conv | status: 404, resp_body: "File cannot be found"}
-  end
-  def handle_file({:error, reason}, conv ) do
-    %{conv | status: 500, resp_body: "File error: #{reason}"}
-  end
-
-  def route(%{ method: "GET", path: "/bears/new" } = conv) do
+  def route(%Conv{ method: "GET", path: "/bears/new" } = conv) do
     @pages_path
     |> Path.join("form.html")
     |> File.read
     |> handle_file(conv)
   end
 
-  def route(%{ method: "GET", path: "/about" } = conv) do
+  def route(%Conv{ method: "GET", path: "/about" } = conv) do
     @pages_path
     |> Path.join("about.html")
     |> File.read
     |> handle_file(conv)
   end
 
-  def route(%{ method: "GET", path: "/wildthings" } = conv) do
+  def route(%Conv{ method: "GET", path: "/wildthings" } = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(%{ method: "GET", path: "/bears"} = conv) do
+  def route(%Conv{ method: "GET", path: "/bears"} = conv) do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
-  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
+  def route(%Conv{ method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(%{path: path} = conv) do
+  def route(%Conv{path: path} = conv) do
       %{conv | status: 404, resp_body: "no #{path} here"}
   end
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     # TODO: Use values in the map to create an HTTP response string:
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
