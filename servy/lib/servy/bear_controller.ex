87f544180879af8233d1d64f5de3947f3fb717a0
Ryan Bryce <ryan.bryce1992@gmail.com>
2019-05-20 15:46:44 -0600

    created a server using erlang gen_tcp learned how to write some tests and how processes work with spawn()

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -3,33 +3,34 @@ defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
 
-  @templates_path Path.expand("templates", File.cwd!)
-
-  defp render(conv, template, bindings \\ []) do
-     contents =
-      @templates_path
-      |> Path.join(template)
-      |> EEx.eval_file(bindings)
-
-     %{ conv | status: 200, resp_body: contents}
-  end
+  @templates_path Path.expand("../../templates", __DIR__)
 
   def index(conv) do
-
     bears =
-    Wildthings.list_bears()
-    |> Enum.sort(&Bear.order_asc_by_name/2)
+      Wildthings.list_bears()
+      |> Enum.sort(&Bear.order_asc_by_name/2)
 
-    render(conv, "index.eex", [bears: bears])
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    render(conv, "show.eex", [bear: bear])
+    render(conv, "show.eex", bear: bear)
   end
 
-  def create(conv, %{"name" => name, "type" => type} = params) do
-    %{conv | status: 201, resp_body: "created a #{type} named #{name}"}
+  def create(conv, %{"name" => name, "type" => type}) do
+    %{ conv | status: 201,
+              resp_body: "Created a #{type} bear named #{name}!" }
   end
+
+  defp render(conv, template, bindings \\ []) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %{ conv | status: 200, resp_body: content }
+  end
+
 end
--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -21,6 +21,16 @@ defmodule Servy.Handler do
       |> format_response
   end
 
+  def route(%Conv{ method: "GET", path: "/kaboom" } = conv) do
+    raise "Kaboom!"
+  end
+
+  def route(%Conv{ method: "GET", path: "/hibernate/" <> time } = conv) do
+    time |> String.to_integer |> :timer.sleep
+
+    %{ conv | status: 200, resp_body: "Awake!" }
+  end
+
   def route(%Conv{ method: "GET", path: "/bears/new" } = conv) do
     @pages_path
     |> Path.join("form.html")
@@ -39,9 +49,14 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
+  def route(%Conv{ method: "GET", path: "/api/bears"} = conv) do
+    Servy.Api.BearController.index(conv)
+  end
+
   def route(%Conv{ method: "GET", path: "/bears"} = conv) do
     BearController.index(conv)
   end
+
   # name=Baloo&type=Brown
   def route(%Conv{ method: "POST", path: "/bears"} = conv) do
     BearController.create(conv, conv.params)
@@ -53,15 +68,15 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{path: path} = conv) do
-      %{conv | status: 404, resp_body: "no #{path} here"}
+      %{conv | status: 404, resp_body: "No #{path} here!"}
   end
   def format_response(%Conv{} = conv) do
     # TODO: Use values in the map to create an HTTP response string:
     """
-    HTTP/1.1 #{Conv.full_status(conv)}
-    Content-Type: text/html
-    Content-Length: #{byte_size(conv.resp_body)}
-
+    HTTP/1.1 #{Conv.full_status(conv)}\r
+    Content-Type: #{conv.resp_content_type}\r
+    Content-Length: #{String.length(conv.resp_body)}\r
+    \r
     #{conv.resp_body}
     """
   end
@@ -78,103 +93,3 @@ defmodule Servy.Handler do
   end
 
 end
-
-  request = """
-  GET /wildthings HTTP/1.1
-  Host: example.com
-  User-Agent: ExampleBrowser/1.0
-  Accept: */*
-
-  """
-
-  response = Servy.Handler.handle(request)
-
-  IO.puts response
-
-  request = """
-  GET /bears HTTP/1.1
-  Host: example.com
-  User-Agent: ExampleBrowser/1.0
-  Accept: */*
-
-  """
-
-  response = Servy.Handler.handle(request)
-
-  IO.puts response
-
-  request = """
-  GET /bigfoot HTTP/1.1
-  Host: example.com
-  User-Agent: ExampleBrowser/1.0
-  Accept: */*
-
-  """
-
-  response = Servy.Handler.handle(request)
-
-  IO.puts response
-
-
-  request = """
-  GET /bears/1 HTTP/1.1
-  Host: example.com
-  User-Agent: ExampleBrowser/1.0
-  Accept: */*
-
-  """
-
-  response = Servy.Handler.handle(request)
-
-  IO.puts response
-
-  request = """
-  GET /wildlife HTTP/1.1
-  Host: example.com
-  User-Agent: ExampleBrowser/1.0
-  Accept: */*
-
-  """
-
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-request = """
-GET /about HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-request = """
-GET /bears/new HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-request = """
-POST /bears HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-Content-Type: application/x-www-form-urlencoded
-Content-Length: 21
-
-name=Baloo&type=Brown
-"""
-
-response = Servy.Handler.handle(request)
-
-IO.puts response
